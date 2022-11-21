prusa3d/Prusa-Firmware-Buddy

commit f10bb8b77d1a4385529f4d2da1bd056817ffd3d0

gui: Make the connect status non-green
Turn the status menu item for connect into a production one, not a
debug-only green item.

BFW-2801.

src/gui/screen_menu_connect.cpp
@@ -27,7 +27,7 @@ class MI_CONNECT_STATUS : public WI_INFO_t {
 
 public:
     MI_CONNECT_STATUS()
-        : WI_INFO_t(_(label), IDR_NULL, is_enabled_t::yes, is_hidden_t::dev) {
+        : WI_INFO_t(_(label), IDR_NULL, is_enabled_t::yes, is_hidden_t::no) {
     }
 };
 
@@ -47,19 +47,19 @@ public:
     virtual void windowEvent(EventLock /*has private ctor*/, window_t *sender, GUI_event_t event, void *param) override {
         if (event == GUI_event_t::CHILD_CLICK || event == GUI_event_t::LOOP) {
             switch (connect_client::last_status()) {
-                S(Off, "Off");
-                S(NoConfig, "No Config");
-                S(NoDNS, "DNS");
-                S(NoConnection, "Refused");
-                S(Tls, "TLS");
-                S(Auth, "Unauthorized");
-                S(ServerError, "Srv error");
-                S(InternalError, "Bug");
-                S(NetworkError, "Net fail");
-                S(Confused, "Protocol err");
-                S(Ok, "Online");
+                S(Off, _("Off"));
+                S(NoConfig, _("No Config"));
+                S(NoDNS, _("DNS error"));
+                S(NoConnection, _("Refused"));
+                S(Tls, _("TLS error"));
+                S(Auth, _("Unauthorized"));
+                S(ServerError, _("Srv error"));
+                S(InternalError, _("Bug"));
+                S(NetworkError, _("Net fail"));
+                S(Confused, _("Protocol err"));
+                S(Ok, _("Online"));
             default:
-                S(Unknown, "Unknown");
+                S(Unknown, _("Unknown"));
             }
         } else {
             SuperWindowEvent(sender, event, param);

src/guiapi/include/WindowMenuInfo.hpp
@@ -54,6 +54,10 @@ public:
         }
     }
 
+    void ChangeInformation(string_view_utf8 str) {
+        str.copyToRAM(information, INFO_LEN - 1);
+    }
+
     virtual void printExtension(Rect16 extension_rect, color_t color_text, color_t color_back, ropfn raster_op) const override {
         printInfo(extension_rect, color_back, _(information));
     }

commit a26435b7fb996b502d01f2a255e3c272e6e0e7ab

Fix invalidation of the connect status menu
In addition, set the status at start too.

Broken in f10bb8b77d1a4385529f4d2da1bd056817ffd3d0.

BFW-3070.

src/gui/screen_menu_connect.cpp
@@ -58,6 +58,25 @@ using Screen = ScreenMenu<GuiDefaults::MenuFooter, MI_RETURN, MI_CONNECT_ENABLED
         break;
 
 class ScreenMenuConnect : public Screen {
+private:
+    void updateStatus() {
+        switch (connect_client::last_status()) {
+            S(Off, _("Off"));
+            S(NoConfig, _("No Config"));
+            S(NoDNS, _("DNS error"));
+            S(NoConnection, _("Refused"));
+            S(Tls, _("TLS error"));
+            S(Auth, _("Unauthorized"));
+            S(ServerError, _("Srv error"));
+            S(InternalError, _("Bug"));
+            S(NetworkError, _("Net fail"));
+            S(Confused, _("Protocol err"));
+            S(Ok, _("Online"));
+        default:
+            S(Unknown, _("Unknown"));
+        }
+    }
+
 public:
     constexpr static const char *label = N_("PRUSA CONNECT");
     ScreenMenuConnect()
@@ -65,21 +84,7 @@ public:
     }
     virtual void windowEvent(EventLock /*has private ctor*/, window_t *sender, GUI_event_t event, void *param) override {
         if (event == GUI_event_t::CHILD_CLICK || event == GUI_event_t::LOOP) {
-            switch (connect_client::last_status()) {
-                S(Off, _("Off"));
-                S(NoConfig, _("No Config"));
-                S(NoDNS, _("DNS error"));
-                S(NoConnection, _("Refused"));
-                S(Tls, _("TLS error"));
-                S(Auth, _("Unauthorized"));
-                S(ServerError, _("Srv error"));
-                S(InternalError, _("Bug"));
-                S(NetworkError, _("Net fail"));
-                S(Confused, _("Protocol err"));
-                S(Ok, _("Online"));
-            default:
-                S(Unknown, _("Unknown"));
-            }
+            updateStatus();
         } else {
             SuperWindowEvent(sender, event, param);
         }

src/guiapi/include/WindowMenuInfo.hpp
@@ -55,7 +55,9 @@ public:
     }
 
     void ChangeInformation(string_view_utf8 str) {
-        str.copyToRAM(information, INFO_LEN - 1);
+        char buffer[INFO_LEN];
+        str.copyToRAM(buffer, INFO_LEN - 1);
+        ChangeInformation(buffer);
     }
 
     virtual void printExtension(Rect16 extension_rect, color_t color_text, color_t color_back, ropfn raster_op) const override {