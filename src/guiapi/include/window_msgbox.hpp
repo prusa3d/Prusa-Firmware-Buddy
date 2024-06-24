prusa3d/Prusa-Firmware-Buddy

commit c67415288b8bb9700fa013a7d0bda8cad099dc2a

Stop using deprecated std::aligned_storage/union
- Replace aligned_storage/union with flat std::array
- As a result, we can remove a lot of includes from some headers

src/guiapi/include/window_msgbox.hpp
@@ -41,7 +41,8 @@ protected:
     // template parameter <PhasesPrintPreview> is irrelevant - same size
     // in case it changes swap <PhasesPrintPreview> with the biggest type
     // it is checked in BindToFSM method
-    using RadioMemSpace = std::aligned_union<0, RadioButton, RadioButtonFsm<PhasesPrintPreview>>::type;
+    static constexpr size_t mem_space_size = sizeof(RadioButtonFsm<PhasesPrintPreview>);
+    using RadioMemSpace = std::array<uint8_t, mem_space_size>;
     RadioMemSpace radio_mem_space;
     static_unique_ptr<IRadioButton> pButtons;
     Response result; // return value
@@ -56,7 +57,8 @@ public:
 
     template <class FSM_PHASE>
     void BindToFSM(FSM_PHASE phase) {
-        static_assert(sizeof(RadioButtonFsm<FSM_PHASE>) <= sizeof(radio_mem_space), "RadioMemSpace is too small");
+        using T = RadioButtonFsm<FSM_PHASE>;
+        static_assert(sizeof(T) <= mem_space_size, "RadioMemSpace is too small");
 
         if (!pButtons) { // pButtons can never be null
             assert("unassigned msgbox");
@@ -71,7 +73,7 @@ public:
 
         // First reset, then create new class; we cannot afford constructing and then destructing because it's the same memory
         pButtons.reset();
-        pButtons = make_static_unique_ptr<RadioButtonFsm<FSM_PHASE>>(&radio_mem_space, this, rc, phase);
+        pButtons = make_static_unique_ptr<T>(radio_mem_space.data(), this, rc, phase);
 
         has_icon ? pButtons->SetHasIcon() : pButtons->ClrHasIcon();
         pButtons->SetBackColor(back);

src/freertos/binary_semaphore.cpp
@@ -8,15 +8,15 @@
 
 namespace freertos {
 
-// If these asserts start failing, go fix the Storage definition
-static_assert(sizeof(BinarySemaphore::Storage) == sizeof(StaticSemaphore_t));
-static_assert(alignof(BinarySemaphore::Storage) == alignof(StaticSemaphore_t));
-
 static SemaphoreHandle_t handle_cast(BinarySemaphore::Storage &semaphore_storage) {
-    return static_cast<SemaphoreHandle_t>(static_cast<void *>(&semaphore_storage));
+    return static_cast<SemaphoreHandle_t>(static_cast<void *>(semaphore_storage.data()));
 }
 
 BinarySemaphore::BinarySemaphore() {
+    // If these asserts start failing, go fix the Storage definition
+    static_assert(semaphore_storage_size == sizeof(StaticSemaphore_t));
+    static_assert(alignof(semaphore_storage) == alignof(StaticSemaphore_t));
+
     SemaphoreHandle_t semaphore = xSemaphoreCreateBinaryStatic(reinterpret_cast<StaticSemaphore_t *>(&semaphore_storage));
     // We are creating static FreeRTOS object here, supplying our own buffer
     // to be used by FreeRTOS. FreeRTOS constructs an object in that memory

src/freertos/include/freertos/binary_semaphore.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <type_traits>
+#include <array>
+#include <cstdint>
+#include <stddef.h>
 
 namespace freertos {
 
@@ -10,13 +13,16 @@ public:
     // We use erased storage in order to not pollute the scope with FreeRTOS internals.
     // The actual size and alignment are statically asserted in implementation file.
 #ifdef UNITTESTS
-    using Storage = std::aligned_storage_t<168, 8>;
+    static constexpr size_t semaphore_storage_size = 168;
+    static constexpr size_t semaphore_storage_align = 8;
 #else
-    using Storage = std::aligned_storage_t<80, 4>;
+    static constexpr size_t semaphore_storage_size = 80;
+    static constexpr size_t semaphore_storage_align = 4;
 #endif
+    using Storage = std::array<uint8_t, semaphore_storage_size>;
 
 private:
-    Storage semaphore_storage;
+    alignas(semaphore_storage_align) Storage semaphore_storage;
 
 public:
     BinarySemaphore();

src/freertos/include/freertos/mutex.hpp
@@ -19,6 +19,10 @@
 #pragma once
 
 #include <type_traits>
+#include <array>
+#include <stddef.h>
+#include <cstdint>
+
 // As tempting as that may be, do not #include <mutex> here because it pulls in
 // a bunch of std::crap which breaks XL debug build due to FLASH inflation.
 
@@ -26,16 +30,20 @@ namespace freertos {
 
 class Mutex {
 public:
-    // We use erased storage in order to not pollute the scope with FreeRTOS internals.
-    // The actual size and alignment are statically asserted in implementation file.
+// We use erased storage in order to not pollute the scope with FreeRTOS internals.
+// The actual size and alignment are statically asserted in implementation file.
 #ifdef UNITTESTS
-    using Storage = std::aligned_storage_t<168, 8>;
+    static constexpr size_t storage_size = 168;
+    static constexpr size_t storage_align = 8;
 #else
-    using Storage = std::aligned_storage_t<80, 4>;
+    static constexpr size_t storage_size = 80;
+    static constexpr size_t storage_align = 4;
 #endif
 
+    using Storage = std::array<uint8_t, storage_size>;
+
 private:
-    Storage mutex_storage;
+    alignas(storage_align) Storage mutex_storage;
 
 public:
     Mutex();

src/freertos/include/freertos/queue.hpp
@@ -3,22 +3,27 @@
 #include <cstddef>
 #include <cstdint>
 #include <type_traits>
+#include <array>
 
 namespace freertos {
 
 // C++ wrapper for FreeRTOS queue.
 class QueueBase {
 public:
-    // We use erased storage in order to not pollute the scope with FreeRTOS internals.
-    // The actual size and alignment are statically asserted in implementation file.
+// We use erased storage in order to not pollute the scope with FreeRTOS internals.
+// The actual size and alignment are statically asserted in implementation file.
 #ifdef UNITTESTS
-    using Storage = std::aligned_storage_t<168, 8>;
+    static constexpr size_t queue_storage_size = 168;
+    static constexpr size_t queue_storage_align = 8;
 #else
-    using Storage = std::aligned_storage_t<80, 4>;
+    static constexpr size_t queue_storage_size = 80;
+    static constexpr size_t queue_storage_align = 4;
 #endif
 
+    using Storage = std::array<uint8_t, queue_storage_size>;
+
 private:
-    Storage queue_storage;
+    alignas(queue_storage_align) Storage queue_storage;
 
 protected:
     QueueBase(size_t item_count, size_t item_size, uint8_t *item_storage);

src/freertos/mutex.cpp
@@ -19,11 +19,11 @@ static_assert(concepts::Lockable<freertos::Mutex>);
 namespace freertos {
 
 // If these asserts start failing, go fix the Storage definition
-static_assert(sizeof(Mutex::Storage) == sizeof(StaticSemaphore_t));
-static_assert(alignof(Mutex::Storage) == alignof(StaticSemaphore_t));
+static_assert(Mutex::storage_size == sizeof(StaticSemaphore_t));
+static_assert(Mutex::storage_align == alignof(StaticSemaphore_t));
 
 static SemaphoreHandle_t handle_cast(Mutex::Storage &mutex_storage) {
-    return static_cast<SemaphoreHandle_t>(static_cast<void *>(&mutex_storage));
+    return static_cast<SemaphoreHandle_t>(static_cast<void *>(mutex_storage.data()));
 }
 
 Mutex::Mutex() {

src/freertos/queue.cpp
@@ -8,15 +8,15 @@
 
 namespace freertos {
 
-// If these asserts start failing, go fix the Storage definition.
-static_assert(sizeof(QueueBase::Storage) == sizeof(StaticQueue_t));
-static_assert(alignof(QueueBase::Storage) == alignof(StaticQueue_t));
-
 static QueueHandle_t handle_cast(QueueBase::Storage &queue_storage) {
-    return static_cast<QueueHandle_t>(static_cast<void *>(&queue_storage));
+    return static_cast<QueueHandle_t>(static_cast<void *>(queue_storage.data()));
 }
 
 QueueBase::QueueBase(size_t item_count, size_t item_size, uint8_t *item_storage) {
+    // If these asserts start failing, go fix the Storage definition.
+    static_assert(queue_storage_size == sizeof(StaticQueue_t));
+    static_assert(alignof(queue_storage) == alignof(StaticQueue_t));
+
     QueueHandle_t queue = xQueueCreateStatic(item_count, item_size, item_storage, reinterpret_cast<StaticQueue_t *>(&queue_storage));
     // We are creating static FreeRTOS object here, supplying our own buffer
     // to be used by FreeRTOS. FreeRTOS constructs an object in that memory

src/gui/MItem_enclosure.cpp
@@ -6,6 +6,8 @@
 #include "ScreenHandler.hpp"
 #include "screen_change_filter.hpp"
 #include <str_utils.hpp>
+#include <screen_menu_enclosure.hpp>
+#include <window_dlg_wait.hpp>
 
 /* Once is Enclosure enabled in menu with ON/OFF switch (MI_ENCLOSURE_ENABLED), it tests the fan and after that Enclosure is declared Active */
 /* If test was passed, MI_ENCLOSURE_ENABLE is swapped with MI_ENCLOSURE and enclosure settings can be accessed */

commit 64c0767ba61332f5e61c1022af6224e8448903d6

Fix radio button mem space
This fixes bug introduced in c67415288b8bb9700fa013a7d0bda8cad099dc2a

src/guiapi/include/window_msgbox.hpp
@@ -41,7 +41,7 @@ protected:
     // template parameter <PhasesPrintPreview> is irrelevant - same size
     // in case it changes swap <PhasesPrintPreview> with the biggest type
     // it is checked in BindToFSM method
-    static constexpr size_t mem_space_size = sizeof(RadioButtonFsm<PhasesPrintPreview>);
+    static constexpr size_t mem_space_size = std::max({ sizeof(RadioButtonFsm<PhasesPrintPreview>), sizeof(RadioButton) });
     using RadioMemSpace = std::array<uint8_t, mem_space_size>;
     alignas(std::max_align_t) RadioMemSpace radio_mem_space;
     static_unique_ptr<IRadioButton> pButtons;