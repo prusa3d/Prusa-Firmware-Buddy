prusa3d/Prusa-Firmware-Buddy

commit 720c785d3999a0ba5c3e3ccbf813d917e60f4d6b

Rewrite FSM queues
Ditch smart queues and dumb queues.
Have the FSM state as the Marlin variable.
Let the clients do the thinking.

BFW-5215

src/state/printer_state.cpp
@@ -1,6 +1,6 @@
 #include "printer_state.hpp"
 
-#include <fsm_types.hpp>
+#include <fsm_states.hpp>
 #include <client_response.hpp>
 #include <marlin_vars.hpp>
 #include <option/has_mmu2.h>
@@ -9,7 +9,6 @@
 #include <device/board.h>
 
 using namespace marlin_server;
-using std::make_optional;
 using std::make_tuple;
 using std::nullopt;
 using std::optional;
@@ -212,8 +211,8 @@ namespace {
         return ErrCode::ERR_UNDEF;
     }
 
-    bool is_warning_attention(fsm::Change &fsm_change) {
-        WarningType wtype = static_cast<WarningType>(*fsm_change.get_data().GetData().data());
+    bool is_warning_attention(const fsm::BaseData &data) {
+        WarningType wtype = static_cast<WarningType>(*data.GetData().data());
         const ErrCode code(warningToErr(wtype));
         switch (code) {
         // Note: We don't consider these attention, so just note the dialog code and slap
@@ -229,20 +228,20 @@ namespace {
         }
     }
 
-    tuple<ErrCode, const Response *> warning_dialog(fsm::Change &top_fsm) {
-        WarningType wtype = static_cast<WarningType>(*top_fsm.get_data().GetData().data());
-        auto phase = GetEnumFromPhaseIndex<PhasesWarning>(top_fsm.get_data().GetPhase());
+    tuple<ErrCode, const Response *> warning_dialog(const fsm::BaseData &data) {
+        WarningType wtype = static_cast<WarningType>(*data.GetData().data());
+        auto phase = GetEnumFromPhaseIndex<PhasesWarning>(data.GetPhase());
         const Response *buttons = ClientResponses::GetResponses(phase).data();
         const ErrCode code(warningToErr(wtype));
         return make_tuple(code, buttons);
     }
 
     // fsm unused on printers, that do not have MMU.
-    optional<ErrCode> load_unload_attention_while_printing([[maybe_unused]] fsm::Change &fsm) {
+    optional<ErrCode> load_unload_attention_while_printing([[maybe_unused]] const fsm::BaseData &data) {
 #if HAS_MMU2()
         if (config_store().mmu2_enabled.get()) {
             // distinguish between regular progress of MMU Load/Unload and a real attention/MMU error screen (which is only one particular FSM state)
-            if (GetEnumFromPhaseIndex<PhasesLoadUnload>(fsm.get_data().GetPhase()) == PhasesLoadUnload::MMU_ERRWaitingForUser) {
+            if (GetEnumFromPhaseIndex<PhasesLoadUnload>(data.GetPhase()) == PhasesLoadUnload::MMU_ERRWaitingForUser) {
                 return ErrCode::CONNECT_MMU_LOAD_UNLOAD_ERROR;
             } else {
                 return nullopt;
@@ -255,23 +254,18 @@ namespace {
 } // namespace
 
 DeviceState get_state(bool ready) {
-    auto [fsm_change, fsm_gen] = marlin_vars()->get_last_fsm_change();
+    const auto &fsm_states = marlin_vars()->get_fsm_states();
+    const auto &top = fsm_states.get_top();
     State state = marlin_vars()->print_state;
-    fsm::Change *top_change = fsm_change.get_top_fsm();
-    if (top_change == nullptr) {
+    if (!top) {
         // No FSM present...
         return get_print_state(state, ready);
     }
 
-    if (top_change->get_fsm_type() == ClientFSM::Warning) {
-        if (is_warning_attention(*top_change)) {
-            return DeviceState::Attention;
-        }
-    }
-
-    switch (top_change->get_fsm_type()) {
+    const fsm::BaseData &data = top->data;
+    switch (top->fsm_type) {
     case ClientFSM::PrintPreview: {
-        auto phase = GetEnumFromPhaseIndex<PhasesPrintPreview>(top_change->get_data().GetPhase());
+        auto phase = GetEnumFromPhaseIndex<PhasesPrintPreview>(data.GetPhase());
         if (attention_while_printpreview(phase)) {
             return DeviceState::Attention;
         }
@@ -281,9 +275,8 @@ DeviceState get_state(bool ready) {
         // NOTE: handled in get_print_state, it can be Printing, Paused or Stopped
         break;
     case ClientFSM::Load_unload:
-        // NOTE: Printing can only be at q0
-        if (fsm_change.q0_change.get_fsm_type() == ClientFSM::Printing) {
-            if (load_unload_attention_while_printing(*top_change)) {
+        if (const fsm::States::State &fsm_state = fsm_states[ClientFSM::Printing]) {
+            if (load_unload_attention_while_printing(*fsm_state)) {
                 return DeviceState::Attention;
             } else {
                 return DeviceState::Printing;
@@ -292,7 +285,7 @@ DeviceState get_state(bool ready) {
             return DeviceState::Busy;
         }
     case ClientFSM::CrashRecovery:
-        if (crash_recovery_attention(GetEnumFromPhaseIndex<PhasesCrashRecovery>(top_change->get_data().GetPhase()))) {
+        if (crash_recovery_attention(GetEnumFromPhaseIndex<PhasesCrashRecovery>(data.GetPhase()))) {
             return DeviceState::Attention;
         }
         break;
@@ -313,9 +306,11 @@ DeviceState get_state(bool ready) {
         // preheat menu to be the only menu screen to not be Idle... :-(
     case ClientFSM::Preheat:
         return DeviceState::Busy;
-        // NOTE: these are here just to satisfy the compiler and get warning, if some cases are not handled.
-        // Both are handled above the switch.
     case ClientFSM::Warning:
+        if (is_warning_attention(data)) {
+            return DeviceState::Attention;
+        }
+        break;
     case ClientFSM::_none:
         break;
     }
@@ -325,38 +320,40 @@ DeviceState get_state(bool ready) {
 StateWithDialog get_state_with_dialog(bool ready) {
     // Get the state and slap top FSM dialog on top of it, if any
     DeviceState state = get_state(ready);
-    auto [fsm_change, fsm_gen] = marlin_vars()->get_last_fsm_change();
-    fsm::Change *top_change = fsm_change.get_top_fsm();
-    if (top_change == nullptr) {
+    const auto &fsm_states = marlin_vars()->get_fsm_states();
+    const auto &fsm_gen = fsm_states.generation;
+    const auto &top = fsm_states.get_top();
+    if (!top) {
         return state;
     }
 
-    switch (top_change->get_fsm_type()) {
+    const auto &data = top->data;
+    switch (top->fsm_type) {
     case ClientFSM::Load_unload:
-        if (fsm_change.q0_change.get_fsm_type() == ClientFSM::Printing) {
-            if (auto attention_code = load_unload_attention_while_printing(*top_change); attention_code.has_value()) {
-                const Response *responses = ClientResponses::GetResponses(GetEnumFromPhaseIndex<PhasesLoadUnload>(top_change->get_data().GetPhase())).data();
+        if (const fsm::States::State &fsm_state = fsm_states[ClientFSM::Printing]) {
+            if (auto attention_code = load_unload_attention_while_printing(*fsm_state); attention_code.has_value()) {
+                const Response *responses = ClientResponses::GetResponses(GetEnumFromPhaseIndex<PhasesLoadUnload>(fsm_state->GetPhase())).data();
                 return { state, attention_code, fsm_gen, responses };
             }
         } // TODO: handle normal load unload
         break;
     case ClientFSM::QuickPause: {
-        const Response *responses = ClientResponses::GetResponses(GetEnumFromPhaseIndex<PhasesQuickPause>(top_change->get_data().GetPhase())).data();
+        const Response *responses = ClientResponses::GetResponses(GetEnumFromPhaseIndex<PhasesQuickPause>(data.GetPhase())).data();
         return { state, ErrCode::CONNECT_QUICK_PAUSE, fsm_gen, responses };
         break;
     }
     case ClientFSM::CrashRecovery:
-        if (auto attention_code = crash_recovery_attention(GetEnumFromPhaseIndex<PhasesCrashRecovery>(top_change->get_data().GetPhase())); attention_code.has_value()) {
-            const Response *responses = ClientResponses::GetResponses(GetEnumFromPhaseIndex<PhasesCrashRecovery>(top_change->get_data().GetPhase())).data();
+        if (auto attention_code = crash_recovery_attention(GetEnumFromPhaseIndex<PhasesCrashRecovery>(data.GetPhase())); attention_code.has_value()) {
+            const Response *responses = ClientResponses::GetResponses(GetEnumFromPhaseIndex<PhasesCrashRecovery>(data.GetPhase())).data();
             return { state, attention_code, fsm_gen, responses };
         }
         break;
     case ClientFSM::Warning: {
-        auto [code, response] = warning_dialog(*top_change);
+        auto [code, response] = warning_dialog(data);
         return { state, code, fsm_gen, response };
     }
     case ClientFSM::PrintPreview: {
-        auto phase = GetEnumFromPhaseIndex<PhasesPrintPreview>(top_change->get_data().GetPhase());
+        auto phase = GetEnumFromPhaseIndex<PhasesPrintPreview>(data.GetPhase());
         if (auto attention_code = attention_while_printpreview(phase); attention_code.has_value()) {
             const Response *responses = ClientResponses::GetResponses(phase).data();
             return { state, attention_code, fsm_gen, responses };
@@ -416,9 +413,9 @@ bool has_job() {
     case DeviceState::Paused:
         return true;
     case DeviceState::Attention: {
-        auto [fsm, fsm_gen] = marlin_vars()->get_last_fsm_change();
+        const auto &fsm_states = marlin_vars()->get_fsm_states();
         // Attention while printing or one of these questions before print(eg. wrong filament)
-        return (fsm.q0_change.get_fsm_type() == ClientFSM::Printing || fsm.q0_change.get_fsm_type() == ClientFSM::PrintPreview);
+        return (fsm_states[ClientFSM::Printing] || fsm_states[ClientFSM::PrintPreview]);
     }
     default:
         return false;

include/common/fsm_base_types.hpp
@@ -36,12 +36,8 @@ public:
         SetPhase(phase);
         SetData(data);
     }
-    constexpr bool operator==(const BaseData &other) const {
-        return GetPhase() == other.GetPhase() && GetData() == other.GetData();
-    }
-    constexpr bool operator!=(const BaseData &other) const {
-        return !((*this) == other);
-    }
+
+    constexpr auto operator<=>(const BaseData &) const = default;
 };
 static_assert(sizeof(BaseData) == BaseDataSZ, "Wrong size of BaseData");
 

src/common/CMakeLists.txt
@@ -18,7 +18,7 @@ if(BOARD MATCHES ".*BUDDY")
             filepath_operation.cpp
             footer_eeprom.cpp
             freertos_mutex.cpp
-            fsm_types.cpp
+            fsm_states.cpp
             general_response.cpp
             hardware_serial.cpp
             heap.cpp
@@ -107,7 +107,7 @@ elseif(BOARD MATCHES "XL_DEV_KIT_XLB")
             data_exchange.cpp
             ExtInterruptHandler.cpp
             freertos_mutex.cpp
-            fsm_types.cpp
+            fsm_states.cpp
             heap.cpp
             homing_reporter.cpp
             hwio_XLBuddy.cpp # todo: replace

src/common/client_fsm_types.h
@@ -117,14 +117,9 @@ enum class WarningType : uint32_t {
     _last = NotDownloaded
 };
 
-// Open dialog has a parameter because I need to set a caption of change filament dialog (load / unload / change).
-// Use extra state of statemachine to set the caption would be cleaner, but I can miss events.
-// Only the last sent event is guaranteed to pass its data.
-using fsm_cb_t = void (*)(uint32_t, uint16_t); // create/destroy/change finite state machine
 using message_cb_t = void (*)(char *);
 #else // !__cplusplus
 // C
-typedef void (*fsm_cb_t)(uint32_t, uint16_t); // create/destroy/change finite state machine
 typedef void (*message_cb_t)(const char *);
 typedef void (*warning_cb_t)(uint32_t);
 #endif //__cplusplus

src/common/filament_sensors_handler.cpp
@@ -108,49 +108,25 @@ void FilamentSensors::for_all_sensors(const std::function<void(IFSensor &sensor,
     }
 }
 
-static ClientFSM type_of_q0 = ClientFSM::_none;
-static ClientFSM type_of_q1 = ClientFSM::_none;
-static ClientFSM type_of_q2 = ClientFSM::_none;
-
-// called when Serial print screen is opened
-// printer is not in sd printing mode, so filament sensor does not trigger M600
-static void fsm_cb(uint32_t u32, uint16_t u16) {
-    fsm::Change change({ u32, u16 });
-    ClientFSM *fsm_type = &type_of_q0;
-
-    // point to variable corresponding to queue from Change
-    switch (change.get_queue_index()) {
-    case fsm::QueueIndex::q0:
-        fsm_type = &type_of_q0;
-        break;
-    case fsm::QueueIndex::q1:
-        fsm_type = &type_of_q1;
-        break;
-    case fsm::QueueIndex::q2:
-        fsm_type = &type_of_q2;
-        break;
-    }
-
-    // fsm created or destroyed
-    if (*fsm_type != change.get_fsm_type()) {
-        if (*fsm_type == ClientFSM::Load_unload) {
-            FSensors_instance().DecEvLock(); // ClientFSM::Load_unload destroy
-        } else if (change.get_fsm_type() == ClientFSM::Load_unload) {
-            FSensors_instance().IncEvLock(); // ClientFSM::Load_unload create
-        }
-        *fsm_type = change.get_fsm_type(); // store new FSM type
-    }
-}
-
 void FilamentSensors::task_init() {
     marlin_client::init();
     marlin_client::wait_for_start_processing();
-    marlin_client::set_event_notify(marlin_server::EVENT_MSK_FSM);
-    marlin_client::set_fsm_cb(fsm_cb);
 }
 
 void FilamentSensors::task_cycle() {
     marlin_client::loop();
+
+    static bool old_state = false;
+    const bool new_state = marlin_vars_t().get_fsm_states().is_active(ClientFSM::Load_unload);
+
+    if (old_state && !new_state) {
+        FSensors_instance().DecEvLock(); // ClientFSM::Load_unload destroy
+    }
+    if (!old_state && new_state) {
+        FSensors_instance().IncEvLock(); // ClientFSM::Load_unload create
+    }
+
+    old_state = new_state;
 
     if (enable_state_update_pending) {
         process_enable_state_update();

src/common/fsm_states.cpp
@@ -1,0 +1,56 @@
+#include <common/fsm_states.hpp>
+
+#include <option/has_phase_stepping.h>
+
+namespace fsm {
+
+static constexpr uint32_t score(ClientFSM fsm_type) {
+    // This roughly correspons to the original FSM queues.
+    // TODO In the future, we could impose a total ordering based
+    //      on the underlying value of the enum.
+    switch (fsm_type) {
+    case ClientFSM::Serial_printing:
+    case ClientFSM::Printing:
+        return 0;
+    case ClientFSM::Load_unload:
+    case ClientFSM::Preheat:
+    case ClientFSM::Selftest:
+    case ClientFSM::ESP:
+    case ClientFSM::CrashRecovery:
+    case ClientFSM::QuickPause:
+    case ClientFSM::PrintPreview:
+    case ClientFSM::ColdPull:
+#if HAS_PHASE_STEPPING()
+    case ClientFSM::PhaseStepping:
+#endif
+        return 1;
+    case ClientFSM::Warning:
+        return 2;
+    case ClientFSM::_none:
+        break;
+    }
+    abort();
+}
+
+std::optional<States::Top> States::get_top() const {
+    std::optional<Top> top;
+
+    size_t i = 0;
+    for (const State &state : states) {
+        const ClientFSM fsm_type = static_cast<ClientFSM>(i++);
+
+        if (!state) {
+            continue;
+        }
+
+        if (top && score(fsm_type) <= score(top->fsm_type)) {
+            continue;
+        }
+
+        top = { .fsm_type = fsm_type, .data = *state };
+    }
+
+    return top;
+}
+
+} // namespace fsm

src/common/fsm_states.hpp
@@ -1,0 +1,43 @@
+#pragma once
+
+#include "client_fsm_types.h"
+#include <array>
+#include <common/fsm_base_types.hpp>
+#include <optional>
+
+namespace fsm {
+
+class States {
+public:
+    // TODO In the future, we could ditch the optional here.
+    //      Instead, each fsm would have 'inactive' phase (some of them already do).
+    using State = std::optional<BaseData>;
+
+private:
+    std::array<State, static_cast<size_t>(ClientFSM::_count)> states;
+
+public:
+    uint32_t generation = 0;
+
+    constexpr bool is_active(ClientFSM client_fsm) const {
+        return (*this)[client_fsm].has_value();
+    }
+
+    constexpr State &operator[](ClientFSM client_fsm) {
+        return states[static_cast<size_t>(client_fsm)];
+    }
+
+    constexpr const State &operator[](ClientFSM client_fsm) const {
+        return states[static_cast<size_t>(client_fsm)];
+    }
+
+    struct Top {
+        ClientFSM fsm_type;
+        BaseData data;
+    };
+    std::optional<Top> get_top() const;
+
+    constexpr auto operator<=>(const States &) const = default;
+};
+
+} // namespace fsm

commit 5dfcb8240356d6e913f349d8e256391908747fb1

connect: Fix reporting MMU runouts
* List all (hopefully) the cases where it asks the user something and
  the user needs to interact.
* More importantly, pass the load/unload FSM to the detection function
  (as it expects that), not the printing (which doesn't contain the
  information about the load/unload). Fixes bug introduced in
  720c785d3999a0ba5c3e3ccbf813d917e60f4d6b (Rewrite FSM queues).

BFW-5952.

src/state/printer_state.cpp
@@ -103,9 +103,15 @@ optional<ErrCode> load_unload_attention_while_printing([[maybe_unused]] const fs
 #if HAS_MMU2()
     if (config_store().mmu2_enabled.get()) {
         // distinguish between regular progress of MMU Load/Unload and a real attention/MMU error screen (which is only one particular FSM state)
-        if (GetEnumFromPhaseIndex<PhasesLoadUnload>(data.GetPhase()) == PhasesLoadUnload::MMU_ERRWaitingForUser) {
+        switch (GetEnumFromPhaseIndex<PhasesLoadUnload>(data.GetPhase())) {
+        case PhasesLoadUnload::MMU_ERRWaitingForUser:
             return ErrCode::CONNECT_MMU_LOAD_UNLOAD_ERROR;
-        } else {
+            // Some other questions... they are the same(ish) as with the non-MMU case, so "rounding up" into the same "error".
+        case PhasesLoadUnload::LoadFilamentIntoMMU:
+        case PhasesLoadUnload::IsColor:
+        case PhasesLoadUnload::IsColorPurge:
+            return ErrCode::CONNECT_FILAMENT_RUNOUT;
+        default:
             return nullopt;
         }
     }
@@ -150,8 +156,8 @@ DeviceState get_state(bool ready) {
         // NOTE: handled in get_print_state, it can be Printing, Paused or Stopped
         break;
     case ClientFSM::Load_unload:
-        if (const fsm::States::State &fsm_state = fsm_states[ClientFSM::Printing]) {
-            if (load_unload_attention_while_printing(*fsm_state)) {
+        if (fsm_states.is_active(ClientFSM::Printing)) {
+            if (load_unload_attention_while_printing(*fsm_states[ClientFSM::Load_unload])) {
                 return DeviceState::Attention;
             } else {
                 return DeviceState::Printing;
@@ -315,8 +321,8 @@ StateWithDialog get_state_with_dialog(bool ready) {
     const auto &data = top->data;
     switch (top->fsm_type) {
     case ClientFSM::Load_unload:
-        if (const fsm::States::State &fsm_state = fsm_states[ClientFSM::Printing]) {
-            if (auto attention_code = load_unload_attention_while_printing(*fsm_state); attention_code.has_value()) {
+        if (fsm_states.is_active(ClientFSM::Printing)) {
+            if (auto attention_code = load_unload_attention_while_printing(*fsm_states[ClientFSM::Load_unload]); attention_code.has_value()) {
                 const Response *responses = ClientResponses::GetResponses(GetEnumFromPhaseIndex<PhasesLoadUnload>(data.GetPhase())).data();
                 return { state, attention_code, fsm_gen, responses };
             }